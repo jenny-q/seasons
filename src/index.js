db36e46a4d6d55bfb3cb32333e41f31a61bf4b69
JennyQ <jennifercquispe@gmail.com>
2020-04-16 20:19:08 -0400

    using componentDidMount, season display update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  //first function called
-  constructor(props) {
-    super(props);
-
-    this.state={
-      lat: null,
-      errorMessage: ''
-    };
+  state = { lat: null, errorMessage: ''};
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          lat: position.coords.latitude,
-          long: position.coords.longitude
-        });
-      },
-      err => {
-        this.setState({ errorMessage: err.message});
-      });
+      position => this.setState({ lat: position.coords.latitude,long: position.coords.longitude }),
+      err => this.setState({ errorMessage: err.message})
+    );
   }
   render() {
-      if( this.state.errorMessage && !this.state.lat) {
+      if(this.state.errorMessage && !this.state.lat) {
         return <div>Error: {this.state.errorMessage}</div>;
       }
       if(!this.state.errorMessage && this.state.lat) {
-        return <div>Latitude: {this.state.lat}</div>;
+        return  <SeasonDisplay lat={this.state.lat} />
       }
       return <div>Loading....</div>
   }
