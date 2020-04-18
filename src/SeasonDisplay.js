63a401e5f2eff25783773fbaae155ca37bba6a97
JennyQ <jennifercquispe@gmail.com>
2020-04-18 16:31:08 -0400

    adding css, spinner, updating season display error

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import './SeasonDisplay.css';
+import React from "react";
+import Spinner from './Spinner';
 
-const SeasonConfig = {
+const seasonConfig = {
     summer: {
         text: "dang it is hot",
         iconName: 'sun'
@@ -13,20 +15,20 @@ const SeasonConfig = {
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
-        return lat > 0 ? 'Summer' : 'Winter';
+    return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat > 0 ? 'Winter' : 'Summer';
+    return lat > 0 ? 'winter' : 'summer';
     }
 }
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const { text, iconName } = SeasonConfig[season];
+  const { text, iconName } = seasonConfig[season];
 
     return (
-        <div>
-            <i className={`${iconName} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 };
